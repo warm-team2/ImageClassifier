597aab30a94936ab2c6edb9ce0755dc130816aec
ChernyshevDmytro <shakal1985@i.ua>
2023-01-15 22:22:50 +0200

    dublicate fix

--- app.py
+++ app.py
@@ -288,40 +288,39 @@ def send_to_google_file():
         if str(item["title"]) == "static":
             global folder_id
             folder_id = item["id"]
-        UPLOAD_FOLDER = os.path.abspath(os.path.join(ful_path, directory))
-        google_files = session.query(GoogleFiles).all()
+    UPLOAD_FOLDER = os.path.abspath(os.path.join(ful_path, directory))
+    google_files = session.query(GoogleFiles).all()
         
-        for google_file in google_files: 
-            if not google_file.file_id:
-                          
-                os.chdir(UPLOAD_FOLDER)
-                gfile = drive.CreateFile(
+    for google_file in google_files: 
+        if not google_file.file_id:                          
+            os.chdir(UPLOAD_FOLDER)
+            gfile = drive.CreateFile(
                         {"parents": [{"kind": "drive#fileLink", "id": folder_id}]}
                     )
-                gfile.SetContentFile(f"{google_file.file_name}.{google_file.file_extension}")
-                gfile.Upload()
-        sleep(15)
-        new_fileList = drive.ListFile({"q": f"'{folder_id}' in parents and trashed=false"}).GetList()
+            gfile.SetContentFile(f"{google_file.file_name}.{google_file.file_extension}")
+            gfile.Upload()
+    sleep(3)
+    new_fileList = drive.ListFile({"q": f"'{folder_id}' in parents and trashed=false"}).GetList()
         #print(new_fileList)
-        google_files1 = session.query(GoogleFiles).all()
-        for google_file1 in google_files1:
-            if not google_file1.file_id:            
-                for files in new_fileList:
+    google_files1 = session.query(GoogleFiles).all()
+    for google_file1 in google_files1:
+        if not google_file1.file_id:            
+            for files in new_fileList:
                     #print(files["title"])
-                    if files["title"] == f"{google_file.file_name}.{google_file.file_extension}":                    
-                        google_file1.file_id = files["id"]       
-                        session.add(google_file)
-                        session.commit()
-            sleep(5)
-            item_path=os.path.join(UPLOAD_FOLDER, f"{google_file1.file_name}.{google_file1.file_extension}")
-            path = pl.Path(item_path)
-            if google_file1.file_id and path.exists():         
-                try:
-                    os.remove(path)
-                except PermissionError:
-                    print(f"trying to delete  {google_file1.file_name}.{google_file1.file_extension}")
-                    os.chdir(current_path)
-                    return redirect("/")
+                if files["title"] == f"{google_file1.file_name}.{google_file1.file_extension}":                    
+                    google_file1.file_id = files["id"]       
+                    session.add(google_file)
+                    session.commit()
+                sleep(5)
+                item_path=os.path.join(UPLOAD_FOLDER, f"{google_file1.file_name}.{google_file1.file_extension}")
+                path = pl.Path(item_path)
+                if google_file1.file_id and path.exists():         
+                    try:
+                        os.remove(path)
+                    except PermissionError:
+                        print(f"trying to delete  {google_file1.file_name}.{google_file1.file_extension}")
+                        os.chdir(current_path)
+                        return redirect("/")
 
         os.chdir(current_path)
     return redirect("/")
